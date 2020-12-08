ac4536a755e7bfa6e9c79b9e934dc3eef8f4da64
Mate Szabolcs Kiss <kissmate@goodiesoft.hu>
2020-12-08 03:24:28 +0100

    Removed portal from modal since its already implemeted that way

--- src/components/BaseModal/BaseModal.tsx
+++ src/components/BaseModal/BaseModal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button, Modal } from "react-bootstrap";
-import { createPortal } from "react-dom";
 import { Theme } from "../../context/ThemeContext";
 import { useTheme } from "../../hooks/useTheme";
 import { BaseModalProps } from "../proptypes";
@@ -10,51 +9,47 @@ export function BaseModal({ data, closeModal, isShown }: BaseModalProps) {
   const { theme } = useTheme();
 
   const handleClose = () => closeModal();
-  const modalPlace = document.getElementById("modal") as Element;
 
-  return createPortal(
-    <>
-      <Modal
-        show={isShown}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-        centered
-        className="neptuna-modal"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>{data.title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{data.body}</Modal.Body>
-        {(data.OkText || data.CancelText) && (
-          <Modal.Footer>
-            {data.OkText && data.OkMethod && (
-              <Button
-                variant={theme === Theme.Default ? "primary" : "dark"}
-                onClick={() => {
-                  if (data.OkMethod) data.OkMethod();
-                  closeModal();
-                }}
-              >
-                {data.OkText}
-              </Button>
-            )}
-            {data.CancelText && data.CancelMethod && (
-              <Button
-                variant={theme === Theme.Default ? "primary" : "dark"}
-                onClick={() => {
-                  if (data.CancelMethod) data.CancelMethod();
-                  closeModal();
-                }}
-              >
-                {data.CancelText}
-              </Button>
-            )}
-          </Modal.Footer>
-        )}
-      </Modal>
-    </>,
-    modalPlace
+  return (
+    <Modal
+      show={isShown}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+      centered
+      className="neptuna-modal"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>{data.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{data.body}</Modal.Body>
+      {(data.OkText || data.CancelText) && (
+        <Modal.Footer>
+          {data.OkText && data.OkMethod && (
+            <Button
+              variant={theme === Theme.Default ? "primary" : "dark"}
+              onClick={() => {
+                if (data.OkMethod) data.OkMethod();
+                closeModal();
+              }}
+            >
+              {data.OkText}
+            </Button>
+          )}
+          {data.CancelText && data.CancelMethod && (
+            <Button
+              variant={theme === Theme.Default ? "primary" : "dark"}
+              onClick={() => {
+                if (data.CancelMethod) data.CancelMethod();
+                closeModal();
+              }}
+            >
+              {data.CancelText}
+            </Button>
+          )}
+        </Modal.Footer>
+      )}
+    </Modal>
   );
 }
 
