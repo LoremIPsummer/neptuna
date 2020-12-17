ecc8de663696de229210b64aba6c752e385426bc
Mate Szabolcs Kiss <kissmate@goodiesoft.hu>
2020-12-17 20:20:33 +0100

    Small pagination bugfix

--- src/hooks/usePagination.tsx
+++ src/hooks/usePagination.tsx
@@ -6,18 +6,16 @@ export function usePagination(items: Array<any>, itemsPerPage? : number) {
   const [pageNumber, setPageNumber] = useState(1);
   const [perPageCount] = useState(itemsPerPage ?? 10);
 
-  const currentPageNumber = pageNumber * perPageCount - perPageCount;
+  const fromItems = pageNumber * perPageCount - perPageCount;
   const allPageCount = items.length / perPageCount;
-  const paginatedItems = items.splice(currentPageNumber, perPageCount);
-
-
+  const paginatedItems = items.splice(fromItems, perPageCount);
 
 
     return {
        PaginatorComponent : () => <Paginator 
        setPage={(val : number) => setPageNumber(val)}
-       next={() => setPageNumber(currentPageNumber < allPageCount ? currentPageNumber+1 : currentPageNumber )}
-       previous={() => setPageNumber(currentPageNumber > 1 ? currentPageNumber-1 : currentPageNumber )}
+       next={() => setPageNumber(pageNumber < allPageCount ? pageNumber+1 : pageNumber )}
+       previous={() => setPageNumber(pageNumber > 1 ? pageNumber-1 : pageNumber )}
        actualPageNum= {pageNumber}
        pageNum= {allPageCount} />,
        items: paginatedItems
